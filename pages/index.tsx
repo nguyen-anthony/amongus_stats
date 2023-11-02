1dbd86adf44e76ee1e0997fd7fc1555942cab2f8
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2023-11-02 09:28:46 -0500

    breakpoints for player stats grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import styles from '@/styles/Home.module.css'
 import {NextPage} from "next";
 import PlayerDropdown from "@/pages/components/PlayerDropdown";
 import {useState} from "react";
+import { Card, CardContent, Grid, Typography } from '@mui/material';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,49 +17,79 @@ export default function Home({players, playerStats, games, topImposters, topDeat
     const filteredStats = selectedPlayer ? playerStats.filter(stats => stats.id === selectedPlayer.id) : playerStats;
 
     return (
-        <div>
-            <h1>Among Us Stats</h1>
-            <div>
-                <h3>General Stats</h3>
-                <p>Total Games Played: {games.games_played}</p>
-                <p>Total Imposter Wins: {games.imposter_wins}</p>
-                <p>Total Crewmate Wins: {games.crewmate_wins}</p>
-            </div>
+        <Grid container direction="column" alignItems="center" spacing={3}>
+            <Grid item xs={12}>
+                <Typography variant="h4" align="center">Among Us Stats</Typography>
+            </Grid>
 
-            <div>
-                <h3>Imposter Wins Leaderboard</h3>
-                {topImposters.map((player: any) => (
-                    <p>{player.name} - {player.wins}</p>
+            <Grid item xs={12}>
+                <Card>
+                    <CardContent>
+                        <Typography variant="h6">General Stats</Typography>
+                        <Typography>Total Games Played: {games.games_played}</Typography>
+                        <Typography>Total Imposter Wins: {games.imposter_wins}</Typography>
+                        <Typography>Total Crewmate Wins: {games.crewmate_wins}</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid container item xs={12} justifyContent="center" spacing={3}>
+                <Grid item xs={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">Imposter Wins Leaderboard</Typography>
+                            {topImposters.map((player: any) => (
+                                <Typography key={player.name}>{player.name} - {player.wins}</Typography>
+                            ))}
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h6">First Deaths Leaderboard</Typography>
+                            {topDeaths.map((player: any) => (
+                                <Typography key={player.name}>{player.name} - {player.deaths}</Typography>
+                            ))}
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12} style={{ width: '50%', margin: '0 auto' }}>
+                <PlayerDropdown players={players} onSelect={(player) => {
+                    if (player && player.id !== -1) {
+                        setSelectedPlayer(player);
+                    } else {
+                        setSelectedPlayer(null);  // Reset when 'All' is selected
+                    }
+                }} />
+            </Grid>
+
+            <Grid container item xs={12} spacing={3}>
+                {filteredStats.map((player: any) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={player.name}> {/* Updated this line */}
+                        <Card>
+                            <CardContent>
+                                <Typography>Player Name: {player.name}</Typography>
+                                <Typography>Games Played: {player.games_played}</Typography>
+                                <Typography>Imposter Wins: {player.games_won_as_imposter}</Typography>
+                                <Typography>
+                                    Imposter Win/Loss Ratio: {player.games_lost_as_imposter !== 0
+                                    ? (player.games_won_as_imposter / player.games_lost_as_imposter).toFixed(2)
+                                    : 'N/A'}
+                                </Typography>
+                                <Typography>Imposter Losses: {player.games_lost_as_imposter}</Typography>
+                                <Typography>First Deaths: {player.times_died_first}</Typography>
+                                <Typography>Imposter Win Percentage:</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 ))}
-            </div>
+            </Grid>
 
-            <div>
-                <h3>First Deaths Leaderboard</h3>
-                {topDeaths.map((player: any) => (
-                    <p>{player.name} - {player.deaths}</p>
-                ))}
-            </div>
-
-            <PlayerDropdown players={players} onSelect={(player) => {
-                if (player && player.id !== -1) {
-                    setSelectedPlayer(player);
-                } else {
-                    setSelectedPlayer(null);  // Reset when 'All' is selected
-                }
-            }} />
-
-
-            {filteredStats.map((player: any) => (
-                <div>
-                    <p>Player Name: {player.name}</p>
-                    <p>Games Played: {player.games_played}</p>
-                    <p>Imposter Wins: {player.games_won_as_imposter}</p>
-                    <p>Imposter Losses: {player.games_lost_as_imposter}</p>
-                    <p>First Deaths: {player.times_died_first}</p>
-                </div>
-            ))}
-
-        </div>
+        </Grid>
     );
 };
 
