d861d113d14df7e0516d7729d34aee89fcce81a2
nguyen-anthony <nguyen.tonyphuc@gmail.com>
2023-11-07 00:19:58 -0600

    encapusulate the YouTube gallery in a card for consistency

--- pages/components/YouTubeGallery.tsx
+++ pages/components/YouTubeGallery.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import {Typography} from "@mui/material"; // Import css
+import { Typography, Card, CardContent } from "@mui/material"; // Import css
 
 type YouTubeGalleryProps = {
     videoIds: string[];
@@ -9,32 +9,34 @@ type YouTubeGalleryProps = {
 
 const YouTubeGallery: React.FC<YouTubeGalleryProps> = ({ videoIds }) => {
     return (
-        <div style={{ maxWidth: '700px', margin: 'auto' }}>
-            <Typography variant="h4" align="center">Video Highlights</Typography>
-            <Carousel
-                showArrows={true}
-                infiniteLoop={true}
-                showStatus={false}
-                showIndicators={true}
-                showThumbs={false}
-                useKeyboardArrows={true}
-                autoPlay={false}
-                dynamicHeight={true}
-            >
-                {videoIds.map((id) => (
-                    <div key={id}>
-                        <iframe
-                            width="100%"
-                            height="400" // You can use dynamic height if you prefer
-                            src={`https://www.youtube.com/embed/${id}`}
-                            frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowFullScreen
-                        ></iframe>
-                    </div>
-                ))}
-            </Carousel>
-        </div>
+        <Card>
+            <CardContent>
+                <Typography variant="h4" align="center">Video Highlights</Typography>
+                <Carousel
+                    showArrows={true}
+                    infiniteLoop={true}
+                    showStatus={false}
+                    showIndicators={true}
+                    showThumbs={false}
+                    useKeyboardArrows={true}
+                    autoPlay={false}
+                    dynamicHeight={true}
+                >
+                    {videoIds.map((id) => (
+                        <div key={id}>
+                            <iframe
+                                width="100%"
+                                height="400" // You can use dynamic height if you prefer
+                                src={`https://www.youtube.com/embed/${id}`}
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                allowFullScreen
+                            ></iframe>
+                        </div>
+                    ))}
+                </Carousel>
+            </CardContent>
+        </Card>
     );
 };
 
